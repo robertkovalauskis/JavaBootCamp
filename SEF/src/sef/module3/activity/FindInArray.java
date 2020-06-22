37ec6a69eba693fa82760b522061253c81505b72
robert <robert.kovalauskis@gmail.com>
2020-06-22 16:57:51 +0300

    Activities Day-2

--- SEF/src/sef/module3/activity/FindInArray.java
+++ SEF/src/sef/module3/activity/FindInArray.java
@@ -14,24 +14,31 @@ public class FindInArray {
 	 */
 	public static void main(String[] args) {
 		
-		// Create an integer array
-		int[] nums = {100, 1, 4, 15};
+//		// Create an integer array
+//		int[] nums = {100, 1, 4, 15};
+//		
+//		int result = nums[0];
+//		
+//		// Find smallest in an array
+//		for (int i = 1; i < nums.length; i++) {
+//			if (nums[i] < result) {
+//				result = nums[i]; // result variable UPDATES!
+//			}
+//		}
+//		System.out.println("Smallest in array is "+result);
 		
-		int result = nums[0];
 		
-		// Find smallest in an array
-		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] < result) {
-				result = nums[i];
-			}
-		}
-		System.out.println("Smallest in array is "+result);
-		
-		result = nums[0];
+		int [] numsT = {10, 15, 20, 120, 1000};
+		int result = numsT[0];
 		
 		// Find largest in an array
-		for (int i = 1; i < nums.length; i++) {
+		for (int i = 1; i < numsT.length; i++) 
+		{
 			// complete the code
+			if(numsT[i] > result) 
+			{
+			result = numsT[i];
+			}
 		}
 		System.out.println("Largest in array is "+result);
 
--- SEF/src/sef/module3/activity/NumToWordsUsingMethod.java
+++ SEF/src/sef/module3/activity/NumToWordsUsingMethod.java
@@ -14,8 +14,10 @@ public class NumToWordsUsingMethod {
 		 */
 		public static void main(String[] args) {
 			// complete the code to print text value of 8, 9 and 10
-
-		}
+			for (int = 8; i <= 10; i++)
+			{
+			printMyWord(i);
+			}
 
 		private static void printMyWord(int i) {
 			final String numText;
