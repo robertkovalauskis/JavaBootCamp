29a4a4b8d4614b3abc0ebc6d187b0db95743e6a4
robert <robert.kovalauskis@gmail.com>
2020-06-25 11:11:31 +0300

    Activities 3d day

--- SEF/src/sef/module6/activity/Employee_I.java
+++ SEF/src/sef/module6/activity/Employee_I.java
@@ -1,24 +1,45 @@
 package sef.module6.activity;
 
 // class Employee_I must extend class Person_I. Complete code for the same
-public class Employee_I  {
+public class Employee_I extends Person_I  {
 	
 	//Attributes
 	private double salary;
 	private String title;
 	
 	//Behavior - write default constructor. Print 'I'm an Employee_I Constructor'
+	public Employee_I()
+	{
+		this.salary = 3000.50;
+		this.title = "tester";
+			
+	}
 	
+	// parametirized + Person_I
+	public Employee_I(double salary, String title, String name, int age){
+		this.salary = salary;
+		this.title = title;
+		this.name = name;
+		this.age = age;
+	}
 	
 	// write getter for int salary
-	
+	public double getSalary() {
+		return salary;
+	}
 	
 	// write setter for int salary
-	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 	
 	// write getter for String title
-	
+	public String getTitle() {
+		return title;
+	}
 	
 	// write setter for String title
-	
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
--- SEF/src/sef/module6/activity/InheritanceActivity.java
+++ SEF/src/sef/module6/activity/InheritanceActivity.java
@@ -3,19 +3,22 @@ package sef.module6.activity;
 
 
 public class InheritanceActivity {
-
+	
+	// setter for String name
+							
 	public static void main(String[] args) {
 		
 		// Create Person object p. Write code to and set name of Person as Sarah Johnson
 		// and age as 21
 		
-		
+		Person_I p = new Person_I("Sarah Johnson", 21);
+	
 		
 		System.out.println("-----------------------------");
 		//Create Employee object e and 
 		// set salary as 70000, title as Developer, age as 32 and name as Shawn Cun
 		
-		
+		Person_I  e = new Employee_I(7000,"Developer", "Shawn Cun", 32);
 		
 		// Print Info using Employee object
 		System.out.println("-----------------------------");
